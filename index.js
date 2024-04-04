a64e2f3cb01e75e44c4fe312f4e4dba4ba7782b4
Rafael de Lara <rafaeldrozdek@gmail.com>
2024-04-04 10:42:11 -0300

    Explaining code line to line

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-import express from "express";
-import path, { dirname } from "path";
-import { fileURLToPath } from 'url';
+import express from "express"; // Importando o express
+import path, { dirname } from "path"; 
+import { fileURLToPath } from 'url';  /* As linhas 2 e 3 importam os modulos 'path', 'dirname' e 'url' para trabalhar com arquivos e caminhos de diretórios */
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url)); /* Atribuindo o caminho do diretório atual a variável '__dirname' */
 
-const app = express();
-const port = 3000;
+const app = express(); // Inicializando o express
+const port = 3000; // Definindo a porta do servidor para testes
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true })); /* Middleware chamado body-parser para receber dados de formulários */
+app.use(express.static(path.join(__dirname, 'public'))); /* Middleware para servir arquivos estáticos (como HTML (no nosso caso EJS), e CSS) */
 
-app.get('/login', (req, res) => {
+app.get('/login', (req, res) => { // Renderiza a página de login usando GET request
     res.render('login.ejs');
-});
+}); 
 
-app.post('/', (req, res) => {
-    const username = req.body.username;
-    const age = new Date().getFullYear() - new Date(req.body.nasc).getFullYear();
-    res.render('index.ejs', { username: username, age: age });
-});
+app.post('/', (req, res) => { /* Renderiza a página inicial usando POST request e passando os dados do formulário */
+    const username = req.body.username;  /* Recebe o nome do usuário do formulário e armazena em uma constante */
+    const age = new Date().getFullYear() - new Date(req.body.nasc).getFullYear(); /* Calcula a idade do usuário a partir da data de nascimento do formulário (usa apenas o ano, passível de bugs) */
+    res.render('index.ejs', { username: username, age: age }); /* Renderiza a página inicial passando o nome do usuário e a idade */
+}); 
 
-app.listen(port, () => {
+app.listen(port, () => { // Inicializa o servidor na porta definida
     console.log(`Server is running on port ${port}`);
 });
